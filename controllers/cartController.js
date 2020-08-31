e84d44d74609318b5975a3f6cb36398893056702
Apurv <apurvsingla@gmail.com>
2020-08-31 18:25:43 +0530

    minor

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,14 +3,17 @@ const Price = require('../models/totalPrice');
 const Money = require('../models/money');
 
 module.exports.addToCart = async (req,res) => {
-    let prevItem = await (await Cart.findOne({price: req.params.price}));
-    if(prevItem){
-        await Cart.updateOne({}, {$inc: {price: req.params.price, quantity: 1}});
-    }else{
-        await Cart.create({price: req.params.price});
-        console.log('sada')
+    if(req.isAuthenticated()){
+        let prevItem = await (await Cart.findOne({price: req.params.price}));
+        if(prevItem){
+            await Cart.updateOne({}, {$inc: {price: req.params.price, quantity: 1}});
+        }else{
+            await Cart.create({price: req.params.price});
+            console.log('sada')
+        }
+        return res.redirect('back');
     }
-    return res.redirect('back');
+    return res.redirect('/auth/login');
 };
 
 
